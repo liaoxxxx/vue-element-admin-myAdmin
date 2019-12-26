22f77d1061d3e76eb3eb476cbb65d2d986978c3c
liaoxx <993501756@qq.com>
2019-12-27 00:55:12 +0800

    2019-12-24    goods list

--- src/views/goods/goods/list.vue
+++ src/views/goods/goods/list.vue
@@ -1,163 +1,161 @@
 <template>
   <div class="login-container">
-    <h1>商品列表</h1>
+    <el-table
+      ref="multipleTable"
+      :data="tableData"
+      tooltip-effect="dark"
+      style="width: 100%"
+      @selection-change="handleSelectionChange"
+    >
+      <el-table-column
+        sortable
+        type="selection"
+        prop="id"
+        width="55"
+      />
+      <el-table-column
+        sortable
+        label="id"
+        prop="id"
+        width="55"
+      />
+      <el-table-column
+        sortable
+        label="商品名称"
+        prop="name"
+        width="160"
+      />
+      <el-table-column
+        sortable
+        label="商品标题"
+        width="240"
+        prop="title"
+      />
+      <el-table-column
+        sortable
+        label="入货价(￥)"
+        width="100"
+        prop="inputPrice"
+      />
+      <el-table-column
+        sortable
+        label="标价(￥)"
+        width="100"
+        prop="showPrice"
+      />
+      <el-table-column
+        sortable
+        label="底价(￥)"
+        width="100"
+        prop="basePrice"
+      />
+      <el-table-column
+        sortable
+        label="描述"
+        width="240"
+        prop="summary"
+      />
+      <el-table-column
+        sortable
+        label="状态"
+        width="120"
+        prop="status"
+      />
+      <el-table-column
+        sortable
+        label="创建日期"
+        width="150"
+        prop="createdAt"
+      />
+      <el-table-column
+        sortable
+        label="修改日期"
+        width="150"
+        prop="updatedAt"
+      />
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="180"
+      >
+        <template slot-scope="scope">
+          <el-button
+            type="text"
+            size="small"
+            @click.native.prevent="delete(scope.$index, tableData)"
+          >
+            删除
+          </el-button>
+          <el-button type="text" size="small" @click="edit(scope.row.id)">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="block">
+      <el-pagination
+        :current-page="currentPage"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Login',
+  name: 'List',
   data() {
     return {
-      loginForm: {
-        username: 'admin',
-        password: '111111'
-      },
-      passwordType: 'password'
+      tableData: [],
+      currentPage: 1
     }
   },
   watch: {
   },
   created() {
-    // window.addEventListener('storage', this.afterQRScan)
+    this.$request.get('/admin_goods/goods_list').then((res) => {
+      res = res.data
+      console.log(res)
+      if (res.status === 1) {
+        this.tableData = res.data
+        for (let i = 0; i < this.tableData.length; i++) {
+          const createdDate = new Date(this.tableData[i].createdAt)
+          const updatedDate = new Date(this.tableData[i].updatedAt)
+          this.tableData[i].createdAt = createdDate.getFullYear() + '-' + createdDate.getUTCMonth() + '-' + createdDate.getUTCDay() + ' ' + createdDate.getUTCHours() + ':' + createdDate.getUTCMinutes() + ':' + createdDate.getUTCSeconds()
+          this.tableData[i].updatedAt = updatedDate.getFullYear() + '-' + updatedDate.getUTCMonth() + '-' + updatedDate.getUTCDay() + ' ' + updatedDate.getUTCHours() + ':' + updatedDate.getUTCMinutes() + ':' + updatedDate.getUTCSeconds()
+        }
+      }
+    })
   },
   mounted() {
-    if (this.loginForm.username === '') {
-      this.$refs.username.focus()
-    } else if (this.loginForm.password === '') {
-      this.$refs.password.focus()
-    }
   },
   destroyed() {
     // window.removeEventListener('storage', this.afterQRScan)
   },
   methods: {
-    checkCapslock({ shiftKey, key } = {}) {
+    edit(id) {
+      this.$router.push({ path: '/goods/category/edit', query: { id: id }})
+    },
+    delete(id) {
+      console.log(id)
+    },
+    handleSelectionChange() {
+      console.log('aaa')
+    },
+    currentPage4() {
+      console.log('aaa')
+    },
+    handleSizeChange() {
+      console.log('aaa')
+    },
+    handleCurrentChange() {
+      console.log('aaa')
     }
+
   }
 }
 </script>
 
-<style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
-$bg:#283443;
-$light_gray:#fff;
-$cursor: #fff;
-
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-  .login-container .el-input input {
-    color: $cursor;
-  }
-}
-
-/* reset element-ui css */
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
-    }
-  }
-
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-}
-</style>
-
-<style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
-
-.login-container {
-  min-height: 100%;
-  width: 100%;
-  background-color: $bg;
-  overflow: hidden;
-
-  .login-form {
-    position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
-  }
-
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
-
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
-  }
-
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
-
-  .title-container {
-    position: relative;
-
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
-    }
-  }
-
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
-  }
-
-  .thirdparty-button {
-    position: absolute;
-    right: 0;
-    bottom: 6px;
-  }
-
-  @media only screen and (max-width: 470px) {
-    .thirdparty-button {
-      display: none;
-    }
-  }
-}
-</style>
